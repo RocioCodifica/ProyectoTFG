5ce81321aea35fa35be0ef586854c720f40771ff
RocioCodifica <rociozarcillera@gmail.com>
2022-07-22 17:04:05 +0200

    Primer Commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="./estilo.css">
+</head>
+
+<body>
+
+    <header> <?php include './header.html'?></header>
+    
+    
+    <main>
+        <div class="container">
+            <div class="grid">
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+                <div class="restaurante">
+                    <div class="imagen">
+                        <img src="./images/210828-200.png" class="grande">
+                    </div>
+                    <div class="nombre">Nombre restaurante</div>
+                </div>
+            </div>
+        </div>
+    </main>
+    
+    
+    
+    <footer>asdasdasdasd</footer>
+
+</body>
+
+</html>
