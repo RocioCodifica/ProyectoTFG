958847d25e1bbf3478f76b66d7723487410ca526
RocioCodifica <rociozarcillera@gmail.com>
2022-08-14 14:08:20 +0200

    Cambios CSS

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel="stylesheet" href="./resetstyles.css">
-    <link rel="stylesheet" href="./estilo.css">
+    <link rel="stylesheet" href="./CSS/resetstyles.css">
+    <link rel="stylesheet" href="./CSS/estilo.css">
 </head>
 
 <body>
